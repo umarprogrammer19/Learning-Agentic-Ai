2c70beec860fd2f64425862f61c3fc263e6c9a35
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-21 11:51:49 +0500

    As Tools

---
+++ 04-OpenAi-Agents-SDK/04-Tools/as_tools.py
@@ -0,0 +1,39 @@
+from agents import Agent, Runner
+from config import config
+import asyncio
+
+spanish_agent = Agent(
+    name="Spanish agent",
+    instructions="You translate the user's message to Spanish",
+)
+
+french_agent = Agent(
+    name="French agent",
+    instructions="You translate the user's message to French",
+)
+
+orchestrator_agent = Agent(
+    name="orchestrator_agent",
+    instructions=(
+        "You are a translation agent. You use the tools given to you to translate."
+        "If asked for multiple translations, you call the relevant tools."
+    ),
+    tools=[
+        spanish_agent.as_tool(
+            tool_name="translate_to_spanish",
+            tool_description="Translate the user's message to Spanish",
+        ),
+        french_agent.as_tool(
+            tool_name="translate_to_french",
+            tool_description="Translate the user's message to French",
+        ),
+    ],
+)
+
+
+result = Runner.run_sync(
+    orchestrator_agent,
+    input="Translate 'Hello, how are you?' in Spanish.",
+    run_config=config,
+)
+print(result.final_output)
