e7d40b7a43f1f1d77a5034a9b89c8f9749152a3d
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-07-24 11:12:06 +0500

    basic config

--- 04-OpenAi-Agents-SDK/06-Context/main.py
+++ 04-OpenAi-Agents-SDK/06-Context/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from 06-context!")
+from agents import Agent, Runner, RunConfig
+from agents.extensions.models.litellm_model import LitellmModel
+from dotenv import get_key, find_dotenv
 
+api_key = get_key(find_dotenv(), "GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+agent = Agent(
+    name="Basic Agent",
+    instructions="you are a helpfull assistant",
+    model=LitellmModel(model="gemini/gemini-2.0-flash", api_key=api_key),
+)
+
+config = RunConfig(tracing_disabled=True)
+
+result = Runner.run_sync(agent, "What is a capital of japan", run_config=config)
+print(result.final_output)
