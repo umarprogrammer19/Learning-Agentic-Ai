6a89ffbca12cad4808975d4ec9da936970389568
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-07-28 00:54:48 +0500

    Input Output Simple

--- 04-OpenAi-Agents-SDK/08-Guardrails/main.py
+++ 04-OpenAi-Agents-SDK/08-Guardrails/main.py
@@ -1,5 +1,12 @@
 from config import config
-from agents import Agent, Runner, input_guardrail
+from agents import (
+    Agent,
+    Runner,
+    input_guardrail,
+    RunContextWrapper,
+    TResponseInputItem,
+    GuardrailFunctionOutput,
+)
 from pydantic import BaseModel
 
 
@@ -31,8 +38,29 @@ math_guardrail_agent = Agent(
     output_type=MathHomeWorkOutput,
 )
 
+
+# @input_guardrail
+# async def math_guardrail(ctx: RunContextWrapper, agent: Agent, input: str | list[TResponseInputItem]) -> GuardailFunctionOutput:
+
 result = Runner.run_sync(
-    guardrail_agent, "what is recursion in python programming", run_config=config
+    math_guardrail_agent,
+    "Find the value of x if 3x + 4 = 9 and x - y = 5 (This is my Math Homework)",
+    run_config=config,
 )
 
 print(result.final_output)
+# If i ask about non math related questions i get these
+
+# input
+# what is recursion in python programming
+
+# output
+# is_math_homework=False reasoning='The user is asking a general question about a programming concept (recursion) in Python, not for help with a specific math problem or equation.'
+
+# and if i ask this
+
+# input
+# Find the value of x if 3x + 4 = 9 and x - y = 5 (This is my Math Homework)
+
+# Output
+# is_math_homework=True reasoning='The user explicitly asks to solve for x in a set of equations and indicates that this is a math homework assignment.'
