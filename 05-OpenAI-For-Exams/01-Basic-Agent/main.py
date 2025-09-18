77cdd2358ef031a50d07ccf87cf6b4c94d44d4ff
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-09-18 13:02:52 +0500

    Basic Agent

--- 05-OpenAI-For-Exams/01-Basic-Agent/main.py
+++ 05-OpenAI-For-Exams/01-Basic-Agent/main.py
@@ -1,6 +1,29 @@
-def main():
-    print("Hello from 01-basic-agent!")
+from agents import Agent, Runner, AsyncOpenAI, OpenAIChatCompletionsModel
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
 
-if __name__ == "__main__":
-    main()
+external_client = AsyncOpenAI(
+    api_key=GEMINI_API_KEY,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client=external_client,
+)
+
+agent = Agent(
+    name="Assistant",
+    instructions="You are a helpfull Assistant",
+    model=model,
+)
+
+result = Runner.run_sync(
+    starting_agent=agent,
+    input="What is the capital of Pakistan",
+)
+
+print(result.final_output)
