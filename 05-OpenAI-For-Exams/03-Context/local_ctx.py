91f89bb89bee7939ea65e89afa0ce12e2f12a6e5
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-09-19 00:01:27 +0500

    Done Context

--- 05-OpenAI-For-Exams/03-Context/local_ctx.py
+++ 05-OpenAI-For-Exams/03-Context/local_ctx.py
@@ -1,5 +1,7 @@
 from agents import Runner, Agent, function_tool, RunContextWrapper
 from dataclasses import dataclass
+import asyncio
+from config import model
 
 
 @dataclass
@@ -16,4 +18,20 @@ async def fetch_user_age(ctx: RunContextWrapper[UserInfo]) -> str:
 
 async def main():
     user_info = UserInfo("Umar Farooq", 281479824)
-    
+
+    agent = Agent(
+        name="Assistant",
+        tools=[fetch_user_age],
+        model=model,
+    )
+
+    result = await Runner.run(
+        starting_agent=agent,
+        input="What is a age of user",
+        context=user_info,
+    )
+
+    print(result.final_output)
+
+
+asyncio.run(main())
