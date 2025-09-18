f0b06fb7be005b6c87892d9d2c0c97f468209cff
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-09-18 23:29:51 +0500

    Update

---
+++ 05-OpenAI-For-Exams/03-Context/llm_ctx_prompt.py
@@ -0,0 +1,19 @@
+from agents import Agent, Runner
+from config import model
+
+agent = Agent(
+    name="Assistant",
+    instructions=(
+        "You are a helpful Assistant"
+        "Extra Information"
+        "The Weather of karachi is 30 Degree"
+    ),
+    model=model,
+)
+
+result = Runner.run_sync(
+    starting_agent=agent,
+    input="What is a Weather of Karachi?",
+)
+
+print(result.final_output)
--- 05-OpenAI-For-Exams/03-Context/main.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from 03-context!")
-
-
-if __name__ == "__main__":
-    main()
