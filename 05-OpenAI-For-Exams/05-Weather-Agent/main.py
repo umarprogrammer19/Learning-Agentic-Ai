10a007b73061171c0ee7e1fb8730420d56ef9d4b
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-09-19 14:08:37 +0500

    Setting

--- 05-OpenAI-For-Exams/05-Weather-Agent/main.py
+++ 05-OpenAI-For-Exams/05-Weather-Agent/main.py
@@ -1,1 +1,12 @@
-from agents import Agent, Runner
+from agents import Agent, Runner, function_tool
+from config import model, WEATHER_API_KEY
+import requests
+
+
+@function_tool
+def fetch_weather(city: str) -> str:
+    """This function fetches the current weather for a given city. It uses the WeatherAPI to get real-time weather data."""
+
+    result = requests.get(
+        f"http://api.weatherapi.com/v1/current.json?key={WEATHER_API_KEY}&q={city}"
+    )
