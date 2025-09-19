233418cfef966ed023c9423866f2c10ee8f89d9c
Umar Farooq <154402323+umarprogrammer19@users.noreply.github.com>
2025-09-19 14:56:15 +0500

    Updated

--- 05-OpenAI-For-Exams/07-Streaming/main.py
+++ 05-OpenAI-For-Exams/07-Streaming/main.py
@@ -3,9 +3,48 @@ from dataclasses import dataclass
 from typing import Callable
 from agents import Agent, Runner, function_tool, RunContextWrapper, ItemHelpers
 from openai.types.responses import ResponseTextDeltaEvent
+from config import model
 
 
 @dataclass
 class UserContext:
     username: str
     email: str | None = None
+
+
+@function_tool()
+async def search(ctx: RunContextWrapper[UserContext], query: str) -> str:
+    import time
+
+    time.sleep(30)
+    return "No results found."
+
+
+def special_prompt(
+    special_context: RunContextWrapper[UserContext],
+    agent: Agent[UserContext],
+):
+    # who is user?
+    # which agent
+    print(f"User: {special_context.context},\n Agent: {agent.name}\n")
+    return f"You are a math expert. User: {special_context.context.username}, Agent: {agent.name}. Please assist with math-related queries."
+
+
+math_agent: Agent = Agent(
+    name="Genius", instructions=special_prompt, model=model, tools=[search]
+)
+
+
+async def call_agent():
+    user_context = UserContext(username="Umar Farooq")
+
+    output = Runner.run_streamed(
+        starting_agent=math_agent,
+        input="search for the best math tutor in my area",
+        context=user_context,
+    )
+    async for event in output.stream_events():
+        print(event)
+
+
+asyncio.run(call_agent())
