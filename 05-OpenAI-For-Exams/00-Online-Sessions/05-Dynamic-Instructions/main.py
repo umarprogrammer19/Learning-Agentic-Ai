2c32dd3254cbd91aa1806ea5a44f8856b9567edf
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-09-21 01:54:44 +0500

    DS

--- 05-OpenAI-For-Exams/00-Online-Sessions/05-Dynamic-Instructions/main.py
+++ 05-OpenAI-For-Exams/00-Online-Sessions/05-Dynamic-Instructions/main.py
@@ -1,6 +1,32 @@
-def main():
-    print("Hello from 05-dynamic-instructions!")
+from agents import Agent, Runner, RunContextWrapper
+from dataclasses import dataclass
+from config import model
 
 
-if __name__ == "__main__":
-    main()
+@dataclass
+class UserContext:
+    name: str
+    uid: int
+
+
+def dynamic_instructions(
+    ctx: RunContextWrapper[UserContext], agent: Agent[UserContext]
+):
+    return f"the user's name is {ctx.context.name}. Help them with thier question"
+
+
+agent = Agent(
+    name="Triage Agent",
+    instructions=dynamic_instructions,
+    model=model,
+)
+
+user_info = UserContext(name="Samiya", uid=872871)
+
+result = Runner.run_sync(
+    agent,
+    "Hey How are you?",
+    context=user_info,
+)
+
+print(result.final_output)
