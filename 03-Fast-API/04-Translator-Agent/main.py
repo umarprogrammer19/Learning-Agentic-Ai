9c501b948e0fa031b3f5414490c9c98b9c3ff43e
umarprogrammer19 <umarprogrammer19666@gmail.com>
2025-09-27 00:36:53 +0500

    Translate Success

--- 03-Fast-API/04-Translator-Agent/main.py
+++ 03-Fast-API/04-Translator-Agent/main.py
@@ -84,7 +84,7 @@ orchestrator = Agent(
 
 if __name__ == "__main__":
     topic = "Photosynthesis"
-    target_lang = "ur"
+    target_lang = "en"
 
     user_prompt = (
         f"Topic: {topic}\n"
